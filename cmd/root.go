c5b3a86921913fd4f11997c1ac18bc591d9e7acc
mricher <maximilien.richer@zenika.com>
2016-07-18 09:54:45 +0200

    Invoke help on empty call

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,8 @@ var RootCmd = &cobra.Command{
 	Long: `A Simple and Flexible scaler for various orchetrators.
 Complete documentation is available at https://github.com/zenika/zscaler/wiki`,
 	Run: func(cmd *cobra.Command, args []string) {
-		_ = cmd.Help()
+		help := cmd.HelpFunc()
+		help(cmd, args)
 	},
 }
 

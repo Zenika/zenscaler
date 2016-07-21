70d2625d8543ccbd36d13b33225b1352cb7e6d99
mricher <maximilien.richer@zenika.com>
2016-07-21 11:36:04 +0200

    Add version command

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,13 @@
 // Package cmd provide the cli informations
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const version = "v0.3-alpha"
 
 // Debug switch
 var Debug bool
@@ -21,5 +27,17 @@ Complete documentation is available at https://github.com/zenika/zscaler/wiki`,
 func init() {
 	RootCmd.AddCommand(StartCmd)
 	RootCmd.AddCommand(DumpConfigCmd)
+	RootCmd.AddCommand(VersionCmd)
 	RootCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "Activate debug output")
+}
+
+// VersionCmd display version number and informations
+var VersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Version number",
+	Long:  "Display version number and build informations",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("zScaler %s\n", version)
+		return
+	},
 }

f8305e1c6b4c151feda4749e9d36a3fdd0303c17
mricher <maximilien.richer@zenika.com>
2016-07-29 10:09:31 +0200

    Test rule creation API

---
+++ api/rules_test.go
@@ -0,0 +1,91 @@
+package api
+
+import (
+	"encoding/json"
+	"testing"
+	"zscaler/core"
+	"zscaler/core/rule"
+	"zscaler/core/scaler"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var MockConf = &core.Configuration{
+	Scalers: map[string]scaler.Scaler{
+		"whoami-compose": &scaler.ComposeScaler{
+			ServiceName: "whoami",
+		},
+	},
+	Rules: map[string]rule.Rule{},
+}
+
+func configAndBuild(input string) error {
+	core.Config = MockConf
+	var frb FloatValueBuilder
+	json.Unmarshal([]byte(input), &frb)
+
+	_, err := frb.Build()
+	return err
+}
+
+func TestCreateComposeRuleSwarmProbe(t *testing.T) {
+	const input = `{
+    "probe": "swarm.cpu_average",
+    "rule": "custom",
+    "scaler": "whoami-compose",
+    "service": "whoami",
+    "probeArgs": {
+        "Tag": "whoami"
+    },
+    "down": "< 1.5",
+    "resfreshRate": 10000000000,
+    "up": "> 2"
+}`
+	err := configAndBuild(input)
+	assert.Nil(t, err)
+}
+
+func TestCreateComposeRuleMissingScaler(t *testing.T) {
+	const input = `{
+    "probe": "swarm.cpu_average",
+    "rule": "custom",
+    "scaler": "missing",
+    "service": "whoami",
+    "probeArgs": {
+        "Tag": "whoami"
+    },
+    "down": "< 1.5",
+    "resfreshRate": 10000000000,
+    "up": "> 2"
+}`
+	err := configAndBuild(input)
+	assert.Error(t, err)
+}
+
+func TestCreateComposeRuleMissingDockerProbeArgs(t *testing.T) {
+	const input = `{
+    "probe": "swarm.cpu_average",
+    "rule": "custom",
+    "scaler": "whoami-compose",
+    "service": "whoami",
+    "down": "< 1.5",
+    "resfreshRate": 10000000000,
+    "up": "> 2"
+}`
+	err := configAndBuild(input)
+	assert.Error(t, err)
+}
+
+func TestCreateComposeRuleBadProbe(t *testing.T) {
+	const input = `{
+    "probe": "bad.probe",
+    "rule": "custom",
+    "scaler": "whoami-compose",
+    "service": "whoami",
+    "down": "< 1.5",
+    "resfreshRate": 10000000000,
+    "up": "> 2"
+}`
+	err := configAndBuild(input)
+	assert.Error(t, err)
+}
